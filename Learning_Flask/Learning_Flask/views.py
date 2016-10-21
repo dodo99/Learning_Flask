8502d6b6a3a7604062599b09893008650304fbf2
dodo99 <tangtony@hotmail.com>
2016-10-20 20:15:59 -0400

    added login, logout capabilities

--- Learning_Flask/Learning_Flask/views.py
+++ Learning_Flask/Learning_Flask/views.py
@@ -3,21 +3,24 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, request
+from flask import render_template, request, session, redirect, url_for
 from Learning_Flask import app
-from Learning_Flask.forms import SignupForm
+from Learning_Flask.forms import SignupForm, LoginForm
 from Learning_Flask.models import db, User
 
 
 @app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+def index():
+  return render_template("index.html")
+
+#@app.route('/home')
+#def home():
+#    """Renders the home page."""
+#    return render_template(
+#        'index.html',
+#        title='Home Page',
+#        year=datetime.now().year,
+#    )
 
 @app.route('/contact')
 def contact():
@@ -54,9 +57,49 @@ def signup():
             newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
             db.session.add(newuser)
             db.session.commit()
-            return 'Success!'
+
+            session['email'] = newuser.email
+            return redirect(url_for('home'))
     elif request.method == 'GET':
         return render_template(
             'signup.html',
             form=form
         )   
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template(
+                'login.html',
+                form=form
+            )
+        else:
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = form.email.data
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+    elif request.method == 'GET':
+        return render_template(
+            'login.html',
+            form=form
+        )   
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/home')
+def home():
+    return render_template(
+        'home.html'
+    )
+
